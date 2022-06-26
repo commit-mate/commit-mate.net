21160fbe350b0d123ebdfdbe2f54d35dda80cf51
mon_sat <tnk@daijiten.info>
2022-06-26 16:32:39 +0900

    feat: dynamic components loader module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,21 +1,21 @@
 import { defineNuxtConfig } from 'nuxt'
+import { fileURLToPath } from 'node:url'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  components: {
-    dirs: [
-      {
-        global: true,
-        path: '~/components/Card',
-        prefix: 'Card',
-      },
-      '~/components',
-    ],
-  },
   modules: [
     '@nuxt/content',
     '@nuxtjs/tailwindcss',
     '@nuxtjs/color-mode',
+    ['./modules/dynamic-component/nuxt', {
+      dirs: [
+        {
+          path: fileURLToPath(new URL('./components/Card', import.meta.url)),
+          global: true,
+          prefix: 'Card',
+        },
+      ],
+    }],
   ],
   // https://color-mode.nuxtjs.org
   colorMode: {
