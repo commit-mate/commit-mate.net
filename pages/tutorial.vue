cc5bfc15d44fd1ab35fdbef29601187bf40b6397
rie-oki-agn <rieoki.style.agn@gmail.com>
2022-06-28 14:40:43 +0900

    remove: 'repositories'

--- pages/tutorial.vue
+++ pages/tutorial.vue
@@ -1,18 +1,21 @@
 <script setup>
-const repositories = ref({
-  origin: [
+const origin = ref(
+  [
     {
       name: 'origin/main',
       commits: []
     }
-  ],
-  local: [
+  ]
+)
+
+const local = ref(
+  [
     {
       name: 'main',
       commits: []
     }
   ]
-})
+)
 
 const createKey = () => {
   return Math.random().toString(32).substring(2)
@@ -34,28 +37,28 @@ const doAdd = () => {
 const commitAble = computed(() => edit.value && add.value ? true : false )
 const doCommit = () => {
   const randomKey = createKey()
-  repositories.value.local[currentBranch.value].commits.push(randomKey)
+  local.value[currentBranch.value].commits.push(randomKey)
   edit.value = false
 }
 
 const doPush = () => {
-  const branchSnap = toRaw(repositories.value.local[currentBranch.value])
+  const branchSnap = toRaw(.local[currentBranch.value])
   branchSnap.name = 'origin/' + branchSnap.name
-  // ↓ it's acting weeiiirrrd, the behavior, as pushing an object to "repositories.origin", it will also push to "repositories.local" as same even if it's raw (not ref() ), is like a bug.
-  repositories.value.origin.push(branchSnap)
+  // ↓ it's acting weeiiirrrd, the behavior, as pushing an object to "origin", it will also push to "local" as same even if it's raw (not ref() ), is like a bug.
+  origin.value.push(branchSnap)
 }
 
 const doSwitchBranch = () => {
-  currentBranch.value == Object.keys(repositories.value.local).length -1
+  currentBranch.value == Object.keys(local.value).length -1
   ? currentBranch.value = 0
   : currentBranch.value += 1
 }
 
 const doCreateBranch = () => {
-  const count = Object.keys(repositories.value.local).length
-  const commitsLength = toRaw(repositories.value.local[currentBranch.value].commits.length)
-  const spacer = toRaw(repositories.value.local[currentBranch.value].spacer)
-  repositories.value.local[count] = {
+  const count = Object.keys(local.value).length
+  const commitsLength = toRaw(local.value[currentBranch.value].commits.length)
+  const spacer = toRaw(.local[currentBranch.value].spacer)
+  local.value[count] = {
     name: `dev-${count}`,
     commits: [],
     spacer: commitsLength + (spacer ? spacer : 0)
@@ -66,13 +69,13 @@ const doCreateBranch = () => {
 
 const clone = ref(false)
 const doClone = () => {
-  repositories.value.local[0].commits.push(currentCommit.value)
+  local.value[0].commits.push(currentCommit.value)
   clone.value = true
 }
 
 onMounted(() => {
   currentCommit.value = createKey()
-  repositories.value.origin[0].commits.push(currentCommit.value)
+  origin.value[0].commits.push(currentCommit.value)
 })
 
 </script>
@@ -93,7 +96,7 @@ onMounted(() => {
           v-for="(branch, index) in repository"
           :key="index"
           class="mb-2 p-2 flex items-center"
-          :class="{'bg-slate-200': branch.name === repositories.local[currentBranch].name}"
+          :class="{'bg-slate-200': branch.name === local[currentBranch].name}"
           >
 
           <div class="w-28">{{branch.name}}</div>
@@ -179,14 +182,14 @@ onMounted(() => {
             type="button"
             class="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-green-500 text-white hover:bg-green-600 focus:outline-none focus:ring-2 focus:ring-green-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800"
             >
-            git push -u origin {{repositories.local[currentBranch].name}}
+            git push -u origin {{local[currentBranch].name}}
           </button>
 
           <button
             @click="doSwitchBranch"
             type="button"
             class="py-3 px-4 inline-flex justify-center items-center gap-2 rounded-md border border-transparent font-semibold bg-blue-500 text-white hover:bg-blue-600 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-all text-sm dark:focus:ring-offset-gray-800">
-            switch {{(currentBranch == Object.keys(repositories.local).length -1) ? repositories.local[0].name : repositories.local[currentBranch+1].name}}
+            switch {{(currentBranch == Object.keys(local).length -1) ? local[0].name : local[currentBranch+1].name}}
           </button>
 
           <button
